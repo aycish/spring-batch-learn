b6d59bc90e64b80b3438252d7b6bb8c056c26e55
aycish <aycish07@gmail.com>
2022-12-06 13:44:06 +0900

    chore : Spring Batch Application 기본 설정 추가

--- src/main/java/com/example/springbatchlearn/SpringBatchApplication.java
+++ src/main/java/com/example/springbatchlearn/SpringBatchApplication.java
@@ -1,13 +1,15 @@
 package com.example.springbatchlearn;
 
+import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SpringBatchLearnApplication {
+@EnableBatchProcessing
+public class SpringBatchApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(SpringBatchLearnApplication.class, args);
+        SpringApplication.run(SpringBatchApplication.class, args);
     }
 
 }
