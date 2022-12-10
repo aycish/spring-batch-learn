bff418b96c108ca37f7fee16b6493c1df5d6e8c5
aycish <aycish07@gmail.com>
2022-12-11 00:02:50 +0900

    feat : JobParameter 도메인 이해를 위한 소스 코드 추가
    - JobParametersBuilder를 통해 Batch Job에 파라미터를 전달하는 방안에 대해서만 Commit
    - 이외의 방법들(java -jar의 파라미터, SpEL)은 노션에 정리

---
+++ src/main/java/com/example/springbatchlearn/domains/JobParameterConfiguration.java
@@ -0,0 +1,73 @@
+package com.example.springbatchlearn.domains;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.JobParameters;
+import org.springframework.batch.core.Step;
+import org.springframework.batch.core.StepContribution;
+import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
+import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
+import org.springframework.batch.core.scope.context.ChunkContext;
+import org.springframework.batch.core.step.tasklet.Tasklet;
+import org.springframework.batch.repeat.RepeatStatus;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.Map;
+
+@RequiredArgsConstructor
+@Configuration
+public class JobParameterConfiguration {
+    private final JobBuilderFactory jobBuilderFactory;
+    private final StepBuilderFactory stepBuilderFactory;
+
+    /*
+    * Job Parameter를 다양한 방법으로 전달하는 것을 테스트한다.
+    * 1. jar 실행 시 전달
+    * 2. JobParameterBuilder로 전달
+    * 3. SpEl로 전달
+    * */
+    @Bean
+    public Job batchJob() {
+        return this.jobBuilderFactory.get("Job")
+                .start(jpStep1())
+                .next(jpStep2())
+                .build();
+    }
+
+    @Bean
+    public Step jpStep1() {
+        return stepBuilderFactory.get("jpStep1")
+                .tasklet(new Tasklet() {
+                    @Override
+                    public RepeatStatus execute(StepContribution contribution, ChunkContext chunkContext) throws Exception {
+                        JobParameters jobParameters = contribution.getStepExecution().getJobExecution().getJobParameters();
+
+                        /* contribution에서는 JobParameters라는 wrapping된 Class로 job parameter에 접근할 수 있다. */
+                        jobParameters.getString("name");
+                        jobParameters.getLong("seq");
+                        jobParameters.getDate("date");
+                        jobParameters.getDouble("age");
+
+                        /* chunkContext에서 가져올 때는 각각의 JobParameter를 까서 Map 형태로 가져온다. */
+                        Map<String, Object> jobParameters1 = chunkContext.getStepContext().getJobParameters();
+
+                        System.out.println("jpStep1 has executed");
+
+                        return RepeatStatus.FINISHED;
+                    }
+                }).build();
+    }
+
+    @Bean
+    public Step jpStep2() {
+        return stepBuilderFactory.get("jpStep2")
+                .tasklet(new Tasklet() {
+                    @Override
+                    public RepeatStatus execute(StepContribution contribution, ChunkContext chunkContext) throws Exception {
+                        System.out.println("jpStep2 has executed");
+                        return RepeatStatus.FINISHED;
+                    }
+                }).build();
+    }
+}
